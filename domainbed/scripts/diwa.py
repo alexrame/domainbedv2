608ce0d813369f6e4b9cd4e1faa333079ab8ee2e
Alexandre RAME <alexandre.rame.cl@gmail.com>
2022-08-01 13:40:50 +0200

    wip

--- domainbed/scripts/diwa.py
+++ domainbed/scripts/diwa.py
@@ -222,6 +222,9 @@ def main():
 
     if os.environ.get("INDOMAIN"):
         dict_domain_to_filter = {"test": "out", "testin": "in"}
+    elif inf_args.topk == 1:
+        # TMP for debugging
+        dict_domain_to_filter = {"test": "out"}
     else:
         dict_domain_to_filter = {"test": "full"}
 
