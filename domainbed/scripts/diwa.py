2c3de320467aed77e0d3ee85be4b60e1897aa608
Alexandre RAME <alexandre.rame.cl@gmail.com>
2022-09-18 21:44:05 +0200

    wip

--- domainbed/scripts/diwa.py
+++ domainbed/scripts/diwa.py
@@ -164,10 +164,10 @@ def load_and_update_networks(wa_algorithm, good_checkpoints, dataset, action="me
         if "model_dict" in save_dict:
             algorithm.load_state_dict(save_dict["model_dict"], strict=False)
         elif checkpoint_weight != -1:
-            print("Load network")
+            print(f"Load network {checkpoint}")
             algorithm.network.load_state_dict(save_dict)
         else:
-            print("Load featurizer")
+            print(f"Load featurizer {checkpoint}")
             algorithm.featurizer.load_state_dict(save_dict)
 
         if checkpoint_weight != -1:
